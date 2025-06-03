eca00f45b0fe9fa71942c6ef119a2c8ea1c8e40c
Hephzibah <hephziboy007@gmail.com>
2025-06-04 02:24:06 +0400

    Add app screenshots to README

---
+++ constants/Screenshots.ts
@@ -0,0 +1,14 @@
+import CheckoutCompletePage from "@/assets/screenshots/CheckoutCompletePage.jpg";
+import favouritesImageChecked from "@/assets/screenshots/favoutitesImageChecked.jpg";
+import homepage from "@/assets/screenshots/homepage.jpg";
+import itemAddedWithToastMessagePage from "@/assets/screenshots/itemAddedWithToastMessagePage.jpg";
+import productDetailsPage from "@/assets/screenshots/productDetailsPage.jpg";
+
+
+export default {
+    CheckoutCompletePage,
+    favouritesImageChecked,
+    homepage,
+    itemAddedWithToastMessagePage,
+    productDetailsPage
+}
